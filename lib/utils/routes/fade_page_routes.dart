18002aa4e0e7c276bc29b998bd74283186768465
Ratn Govindam <rg.ratn@gmail.com>
2024-07-02 17:25:59 +0530

    Fade Animation Page

---
+++ lib/utils/routes/fade_page_routes.dart
@@ -0,0 +1,22 @@
+import 'package:authify/pages/home_page.dart';
+import 'package:flutter/material.dart';
+
+class FadePageRoute extends PageRouteBuilder {
+  final Widget _child;
+  FadePageRoute(this._child)
+      : super(
+          transitionDuration: const Duration(milliseconds: 500),
+          transitionsBuilder: (BuildContext context,
+              Animation<double> animation,
+              Animation<double> secondAnimation,
+              Widget child) {
+            return FadeTransition(
+              opacity: animation,
+              child: child,
+            );
+          },
+          pageBuilder: (BuildContext context, animation, secondAnimation) {
+            return HomePage();
+          },
+        );
+}
